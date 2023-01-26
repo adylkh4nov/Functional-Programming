c8fe352087e99986b5e585e5fc124ee0849448e2
Olzhas <adylkh4nov@gmail.com>
2023-01-26 11:29:02 +0600

    FP laba#2 1 commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Введите имя")
+data = str(input())
+print("Введите возраст")
+age = int(input())
+def Smoke(data):
+    if(age >= 18):
+        print(f"{data} Ты можешь курить")
+    else:
+        print(f"{data} Ты не можешь курить")
+
+Smoke(data)
